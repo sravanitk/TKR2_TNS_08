df37ff8b7c71d4e516cf2b0b42e23f97b2d26080
sravanitk <145426117+sravanitk@users.noreply.github.com>
2023-10-16 13:13:14 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,12 @@
+
+
+
+package com.oop1.constructor;
+
+public class Car 
+	public Car() {
+		
+		//Code block constructor general
+	}
+
+}
---
+++ Car1.java
@@ -0,0 +1,60 @@
+
+
+package com.oop;
+
+public class Car1 {
+	
+	private String doors;
+	private String engine;
+	private String drivers;
+	public int speed ;
+	/**
+	 * @return the doors
+	 */
+	public String getDoors() {
+		return doors;
+	}
+	/**
+	 * @param doors the doors to set
+	 */
+	public void setDoors(String doors) {
+		this.doors = doors;
+	}
+	/**
+	 * @return the engine
+	 */
+	public String getEngine() {
+		return engine;
+	}
+	/**
+	 * @param engine the engine to set
+	 */
+	public void setEngine(String engine) {
+		this.engine = engine;
+	}
+	/**
+	 * @return the drivers
+	 */
+	public String getDrivers() {
+		return drivers;
+	}
+	/**
+	 * @param drivers the drivers to set
+	 */
+	public void setDrivers(String drivers) {
+		this.drivers = drivers;
+	}
+	/**
+	 * @return the speed
+	 */
+	public int getSpeed() {
+		return speed;
+	}
+	/**
+	 * @param speed the speed to set
+	 */
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+}
